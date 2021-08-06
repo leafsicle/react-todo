fa4171098ed6d7aab2af1adc99d0aeb8bd36fa78
Matt <cookem529@gmail.com>
2021-08-06 14:35:00 -0400

    Add is now a toggle!

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,11 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
-  let onClick = () => {
-    console.log('clicked on +')
-  }
-
+const Header = ({ title, onAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick} />
+      <Button color='green' text='Add' onClick={onAdd} />
     </header>
   )
 }
